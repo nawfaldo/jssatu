e824788ae884ddffec93f825bd32393d45c80336
nawfaldo <nawfaldo.main@gmail.com>
2023-03-07 15:06:31 +0700

    re

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,39 +1,39 @@
-/** @type {import("eslint").Linter.Config} */
-const config = {
-  overrides: [
-    {
-      extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-      files: ["*.ts", "*.tsx"],
-      parserOptions: {
-        project: "tsconfig.json",
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.json",
-  },
-  plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-  rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-floating-promises": "off",
-    "@typescript-eslint/no-unsafe-assignment": "off",
-    "@typescript-eslint/no-unsafe-member-access": "off",
-    "@typescript-eslint/restrict-template-expressions": "off",
-    "@typescript-eslint/no-unsafe-return": "off",
-    "@typescript-eslint/no-unsafe-call": "off",
-  },
-};
+// /** @type {import("eslint").Linter.Config} */
+// const config = {
+//   overrides: [
+//     {
+//       extends: [
+//         "plugin:@typescript-eslint/recommended-requiring-type-checking",
+//       ],
+//       files: ["*.ts", "*.tsx"],
+//       parserOptions: {
+//         project: "tsconfig.json",
+//       },
+//     },
+//   ],
+//   parser: "@typescript-eslint/parser",
+//   parserOptions: {
+//     project: "./tsconfig.json",
+//   },
+//   plugins: ["@typescript-eslint"],
+//   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+//   // rules: {
+//   //   "@typescript-eslint/consistent-type-imports": [
+//   //     "warn",
+//   //     {
+//   //       prefer: "type-imports",
+//   //       fixStyle: "inline-type-imports",
+//   //     },
+//   //   ],
+//   //   "@typescript-eslint/no-explicit-any": "off",
+//   //   "@typescript-eslint/no-unused-vars": "off",
+//   //   "@typescript-eslint/no-floating-promises": "off",
+//   //   "@typescript-eslint/no-unsafe-assignment": "off",
+//   //   "@typescript-eslint/no-unsafe-member-access": "off",
+//   //   "@typescript-eslint/restrict-template-expressions": "warn",
+//   //   "@typescript-eslint/no-unsafe-return": "off",
+//   //   "@typescript-eslint/no-unsafe-call": "off",
+//   // },
+// };
 
-module.exports = config;
+// module.exports = config;
