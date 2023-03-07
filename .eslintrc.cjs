5dc0989c357e9847a579ae2a2a9a3854a91eae04
nawfaldo <nawfaldo.main@gmail.com>
2023-03-07 15:03:38 +0700

    re

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,6 +27,12 @@ const config = {
     ],
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
   },
 };
 
