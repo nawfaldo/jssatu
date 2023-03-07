40fca2d1a35e5c93aca3d4049a3f306695046829
nawfaldo <nawfaldo.main@gmail.com>
2023-03-07 14:59:41 +0700

    re

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,33 @@
-/** @type {import("eslint").Linter.Config} */
-const config = {
-  overrides: [
-    {
-      extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-      files: ["*.ts", "*.tsx"],
-      parserOptions: {
-        project: "tsconfig.json",
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.json",
-  },
-  plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-  rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
-    "@typescript-eslint/no-explicit-any": "off",
-    "@@typescript-eslint/no-unused-vars": "off",
-  },
-};
+// /** @type {import("eslint").Linter.Config} */
+// const config = {
+//   overrides: [
+//     {
+//       extends: [
+//         "plugin:@typescript-eslint/recommended-requiring-type-checking",
+//       ],
+//       files: ["*.ts", "*.tsx"],
+//       parserOptions: {
+//         project: "tsconfig.json",
+//       },
+//     },
+//   ],
+//   parser: "@typescript-eslint/parser",
+//   parserOptions: {
+//     project: "./tsconfig.json",
+//   },
+//   plugins: ["@typescript-eslint"],
+//   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+//   rules: {
+//     "@typescript-eslint/consistent-type-imports": [
+//       "warn",
+//       {
+//         prefer: "type-imports",
+//         fixStyle: "inline-type-imports",
+//       },
+//     ],
+//     "@typescript-eslint/no-explicit-any": "off",
+//     "@@typescript-eslint/no-unused-vars": "off",
+//   },
+// };
 
-module.exports = config;
+// module.exports = config;
